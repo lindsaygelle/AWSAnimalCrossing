4797702e75cb93e32afcf1251fec73243b51ae5a
lindsaygelle <lindsay.gelle@gmail.com>
2023-03-13 10:02:28 +1100

    Added development stage for the REST API

--- Lambda/villager/get/lambda.py
+++ Lambda/villager/get/lambda.py
@@ -1,5 +1,7 @@
 def lambda_handler(event: str, context: dict) -> dict:
     response: dict = {
-        "body": "Hello!",
+        "body": {
+            "animal": "Mockingbird",
+            "name": "Mock"},
         "statusCode": 200}
     return response
